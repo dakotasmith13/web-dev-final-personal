8c2e7241415180f55c36f52ca9795d3ced37b0d7
khughes2 <97975072+khughes2@users.noreply.github.com>
2022-05-02 17:07:40 -0400

    Add files via upload

---
+++ sql.php
@@ -0,0 +1,27 @@
+<?php
+    include 'top.php';
+?>
+        <main>
+            <h1>SQL</h1>
+            <h2>Create table</h2>
+            <pre>
+                CREATE TABLE tblMiataSuggestionsForm(
+                    pmkMiataSuggestionsFormID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    fldFirstName VARCHAR(25),
+                    fldAge VARCHAR(3),
+                    fldEmail VARCHAR(30),
+                    fldCarEnthusiast VARCHAR(15),
+                    fldMiataSuggestions TEXT
+                )
+            </pre>
+            <h2>Insert Data</h2>
+            <pre>
+            INSERT INTO tblMiataSuggestionsForm
+                (fldFirstName, fldAge, fldEmail, fldCarEnthusiast, fldMiataSuggestions)
+            VALUES (?, ?, ?, ?, ?)
+            </pre>
+        </main>
+
+<?php
+    include 'footer.php';
+?>
