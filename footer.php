a76ed04c3d4576ca9cf6da8a37340370ca362857
dakotasmith13 <97972068+dakotasmith13@users.noreply.github.com>
2022-05-02 22:56:20 -0400

    Final project: added copyright

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
             <p><a href="../index.php">Site map (Main index)</a></p>
             <p><a href="//khughes2.w3.uvm.edu/cs008/final">Kat Hughes- Section OL</a></p>
             <p><a href="//dsmith13.w3.uvm.edu/cs008/final">Dakota Smith- Section OL</a></p>
+            <p style="text-align: center">&copy;Copyright 2022 Kat & Dak</p>
         </footer> 
     </body>
 </html>
