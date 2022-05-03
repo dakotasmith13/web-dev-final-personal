1668b18156493097fe696790a2f2c6a3a98c5e6c
dakotasmith13 <97972068+dakotasmith13@users.noreply.github.com>
2022-05-03 10:17:49 -0400

    Final project: updated header

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
         <a href="../final/index.php"><img src="../final/images/logo.png" alt="logo"></a>
     </section>
     <section class="title">
-        <h1>Project: Miata</h1>
+        <a href="../final/index.php"><h1>Project: Miata</h1></a>
     </section>
     <section class="top-nav">
         <h2 hidden>Top</h2>
